7b7077c3022f115432dd382df7c4baf028163d5f
Tom Brodhurst-Hill <developer@barefeetware.com>
2020-12-14 12:12:41 +1100

    Initial Commit

---
+++ BFWViews/BFWViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BFWViewsApp.swift
+//  BFWViews
+//
+//  Created by Tom Brodhurst-Hill on 14/12/20.
+//
+
+import SwiftUI
+
+@main
+struct BFWViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
