c520cf82ea23a8f67a60568cda1c46337e5064ad
Tom Brodhurst-Hill <developer@barefeetware.com>
2021-09-25 17:15:50 +1000

    ImageLoader module: moved and split files

---
+++ BFWViews/Modules/ImageLoader/ViewModel/ImageLoaderView.ViewModel.swift
@@ -0,0 +1,55 @@
+//
+//  ImageLoaderView.ViewModel.swift
+//  BFWViews
+//
+//  Created by Tom Brodhurst-Hill on 25/9/21.
+//  Copyright © 2021 BareFeetWare. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+extension ImageLoaderView {
+    class ViewModel: ObservableObject {
+        
+        init(url: URL) {
+            self.url = url
+        }
+        
+        let url: URL
+        @Published var imageData: Data?
+        
+        private var subscribers = Set<AnyCancellable>()
+    }
+}
+
+extension ImageLoaderView.ViewModel {
+    
+    func onAppear() {
+        if subscribers.isEmpty {
+            subscribe()
+        }
+    }
+
+}
+
+private extension ImageLoaderView.ViewModel {
+
+    func subscribe() {
+        Fetcher.dataPublisher(url: url)
+            .receive(on: DispatchQueue.main)
+            .sink(
+                receiveCompletion: { completion in
+                    switch completion {
+                    case .failure(let error): debugPrint("error = \(error)")
+                    case .finished: break
+                    }
+                },
+                receiveValue: {
+                    self.imageData = $0
+                }
+            )
+            .store(in: &subscribers)
+    }
+    
+}
