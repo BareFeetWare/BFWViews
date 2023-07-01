a18d922932b045cc6cbfb779d6164e6817a145a9
Tom Brodhurst-Hill <developer@barefeetware.com>
2023-07-01 17:22:44 +1000

    PassedObject: Observable class/object wrapper to pass/share between views.

---
+++ BFWViews/Modules/Common/Model/PassedObject.swift
@@ -0,0 +1,19 @@
+//
+//  PassedObject.swift
+//  BFWViews
+//
+//  Created by Tom Brodhurst-Hill on 1/7/2023.
+//  Copyright © 2023 BareFeetWare. All rights reserved.
+//
+
+import Foundation
+
+/// Observable class/object wrapper to pass/share between views.
+public class PassedObject<Value>: ObservableObject {
+    
+    public init(value: Value) {
+        self.value = value
+    }
+    
+    @Published public var value: Value
+}
