0b85245e951ef32c642d316770801d294f54c379
Tom Brodhurst-Hill <developer@barefeetware.com>
2023-12-31 15:38:44 +1100

    Observing: Consolidated observing of two objects into one wrapper.

--- BFWViews/Modules/Common/View/Observing.swift
+++ BFWViews/Modules/Common/View/Observing.swift
@@ -8,18 +8,33 @@
 import SwiftUI
 
 /// Wrap a view that needs to update when the `observed` object changes. Useful when injecting a view init into a view hierarchy.
-public struct Observing<Content: View, Observed: ObservableObject> {
+public struct Observing<Content: View, Observed1: ObservableObject, Observed2: ObservableObject> {
     
     public init(
-        _ observed: Observed,
+        _ observed1: Observed1,
+        _ observed2: Observed2,
         @ViewBuilder content: @escaping () -> Content
     ) {
-        self.observed = observed
+        self.observed1 = observed1
+        self.observed2 = observed2
         self.content = content
     }
     
-    @ObservedObject var observed: Observed
+    @ObservedObject var observed1: Observed1
+    @ObservedObject var observed2: Observed2
     @ViewBuilder let content: () -> Content
+}
+
+public extension Observing where Observed2 == EmptyObserved {
+    
+    init(
+        _ observed1: Observed1,
+        @ViewBuilder content: @escaping () -> Content
+    ) {
+        self.observed1 = observed1
+        self.observed2 = EmptyObserved()
+        self.content = content
+    }
 }
 
 extension Observing: View {
@@ -27,3 +42,5 @@ extension Observing: View {
         content()
     }
 }
+
+public class EmptyObserved: ObservableObject {}
