6ffea4f58008fb4e2ab7fcebbd3a3e17876af1cc
Tom Brodhurst-Hill <developer@barefeetware.com>
2024-01-24 09:12:33 +1100

    Plan.Confirmation: added

---
+++ BFWViews/Modules/Common/Model/Binding+Transform.swift
@@ -0,0 +1,39 @@
+//
+//  Binding+Transform.swift
+//  BFWViews
+//
+//  Created by Tom Brodhurst-Hill on 23/1/2024.
+//  Copyright © 2024 BareFeetWare. All rights reserved.
+//
+
+import SwiftUI
+
+public protocol OptionalProtocol {
+    associatedtype Wrapped
+    var optional: Wrapped? { get set }
+}
+
+extension Optional: OptionalProtocol {
+    public var optional: Wrapped? {
+        get {
+            self
+        }
+        set {
+            self = newValue
+        }
+    }
+}
+
+public extension Binding where Value: OptionalProtocol {
+
+    var isNotNil: Binding<Bool> {
+        .init {
+            wrappedValue.optional != nil
+        } set: {
+            if !$0 {
+                wrappedValue.optional = nil
+            }
+        }
+    }
+    
+}
---
+++ BFWViews/Modules/Common/View/Plan.Confirmation.swift
@@ -0,0 +1,91 @@
+//
+//  Plan.Confirmation.swift
+//  BFWViews
+//
+//  Created by Tom Brodhurst-Hill on 23/1/2024.
+//  Copyright © 2024 BareFeetWare. All rights reserved.
+//
+
+import SwiftUI
+
+extension Plan {
+    public struct Confirmation {
+        public let title: String
+        public let buttonTitle: String
+        public let action: () -> Void
+        
+        public init(
+            title: String,
+            buttonTitle: String,
+            action: @escaping () -> Void
+        ) {
+            self.title = title
+            self.buttonTitle = buttonTitle
+            self.action = action
+        }
+    }
+}
+
+extension View {
+    
+    @ViewBuilder
+    public func confirmationDialog(
+        _ confirmationBinding: Binding<Plan.Confirmation?>
+    ) -> some View {
+        if let confirmation = confirmationBinding.wrappedValue {
+            self.confirmationDialog(
+                confirmation.title,
+                isPresented: confirmationBinding.isNotNil
+            ) {
+                Button(confirmation.buttonTitle, role: .none) {
+                    confirmation.action()
+                }
+                Button("Cancel", role: .cancel) {}
+            }
+        } else {
+            self
+        }
+    }
+    
+}
+
+struct PlanConfirmationDialog_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            Preview()
+                .navigationTitle("Plan.Confirmation")
+        }
+    }
+    
+    struct Preview: View {
+        @State var confirmation: Plan.Confirmation?
+        @State var result: String = "None"
+        
+        var body: some View {
+            List {
+                HStack {
+                    Text("Result:")
+                    Spacer()
+                    Text(result)
+                }
+                Button("Do First") {
+                    confirmation = .init(
+                        title: "Do First?",
+                        buttonTitle: "First"
+                    ) {
+                        self.result = "First"
+                    }
+                }
+                Button("Do Second") {
+                    confirmation = .init(
+                        title: "Do Second?",
+                        buttonTitle: "Second"
+                    ) {
+                        self.result = "Second"
+                    }
+                }
+            }
+            .confirmationDialog($confirmation)
+        }
+    }
+}
